fc260f1bb3faf389fc42538211353525380e589d
Yoshi1900 <177987679+Yoshi1900@users.noreply.github.com>
2024-10-30 05:21:32 +0000

    コメント系の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :playgrounds, dependent: :destroy
   has_many :posts, dependent: :destroy
-         
+  has_many :post_comments, dependent: :destroy 
+  has_one_attached :avatar_image
+  
+  GUEST_USER_EMAIL = "guest@example.com"
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+    
+  def guest_user?
+    email == GUEST_USER_EMAIL
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,35 +2,42 @@ Rails.application.routes.draw do
   
   namespace :admin do
     root to: 'admin#home'
-    resources :users, only: [:index,:edit,:show,:update,:destroy]
-    resources :tags, only: [:index,:edit,:update,:destroy]
-    resources :posts, only: [:index,:show,:edit,:update,:destroy] 
-    resources :playgrounds, only: [:index,:new,:create,:show,:edit,:update,:destroy]
+    resources :users, only: [:index, :edit, :show, :update, :destroy]
+    resources :tags, only: [:index, :edit, :update, :destroy]
+    resources :posts, only: [:index, :show, :edit, :update, :destroy] do
+      resources :post_comments, only: [:create,:destroy]
+    end
+    resources :playgrounds, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   end
+  
 
-
-  namespace :admin do
+   scope module: :public do
     root to: "homes#top"
-    get "about", to:"homes#about"
-    resources :posts, only: [:index,:new,:show,:create,:destroy]
-    resources :playgrounds, only: [:new,:index,:show,:create,:edit,:update,:destroy]
+    get "about", to: "homes#about"
+    resources :posts, only: [:index, :new, :show, :create, :destroy] do
+      resources :post_comments, only: [:create,:destroy]
+    end
+    resources :playgrounds, only: [:new, :index, :show, :create, :edit, :update, :destroy]
     get 'users/my_page' => 'users#show'
     get 'users/information/edit' => 'users#edit'
     patch 'users/information' => 'users#update'
     get 'users/unsubscribe' => 'users#unsubscribe'
-    patch 'users/withdraw' => 'userrs#withdraw'
+    patch 'users/withdraw' => 'users#withdraw'
   end
 
+  get '/search' => 'searches#search'
 
-  get '/search' => 'seaches#search'
-
-  end
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
   }
   devise_for :users, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
   }
+  
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+  
 
 end
