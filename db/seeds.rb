f422c47ef1591446c1010977f75495df8e1c6fbc
EC2 Default User <ec2-user@ip-172-31-41-40.ap-northeast-1.compute.internal>
2024-11-05 07:07:01 +0000

    seedファイルの編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
 end
 
 Playground.find_or_create_by!(name: "大城遊園地") do |playground|
-  playground.playground_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  playground.playground_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/playground1.jpg"), filename:"playground1.jpg")
   playground.description = "大人気の遊園地"
   playground.post_code = "1111111"
   playground.address = "富山県高岡市大城町123-12"
@@ -42,7 +42,7 @@ Playground.find_or_create_by!(name: "大城遊園地") do |playground|
 end
 
 Playground.find_or_create_by!(name: "せん動物園") do |playground|
-  playground.playground_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
+  playground.playground_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/playground2.jpg"), filename:"playground2.jpg")
   playground.description = "美しい動物園"
   playground.post_code = "2222222"
   playground.address = "富山県富山市戦場ヶ原町233-23"
@@ -50,7 +50,7 @@ Playground.find_or_create_by!(name: "せん動物園") do |playground|
 end
 
 Playground.find_or_create_by!(name: "魚川ファインモール") do |playground|
-  playground.playground_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
+  playground.playground_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/splayground3.jpg"), filename:"playground3.jpg")
   playground.description = "定期的に様々なイベントを行っているショッピングモール"
   playground.post_code = "3333333"
   playground.address = "富山県魚川立浪町784-32"
