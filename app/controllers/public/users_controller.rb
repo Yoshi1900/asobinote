f3e440c1e3cfe8c394b121f3895309f514166693
EC2 Default User <ec2-user@ip-172-31-41-40.ap-northeast-1.compute.internal>
2024-11-10 02:04:16 +0000

    非ログイン時の対応

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,16 +1,17 @@
 class Public::UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:mypage, :edit, :update]
+  before_action :authenticate_user!, only: [:mypage, :show, :edit, :update]
   before_action :set_user, only: [:show, :edit, :update]
   before_action :ensure_guest_user, only: [:edit]
   
 
   def edit
     unless @user == current_user
-      redirect_to user_path(@user) #ログインユーザー以外のページだとマイページにリダイレクト
+      redirect_to mypage_path #ログインユーザー以外のページだとマイページにリダイレクト
     end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def mypage
@@ -18,6 +19,9 @@ class Public::UsersController < ApplicationController
   end
 
   def update
+    unless @user == current_user
+      redirect_to user_path(@user) #ログインユーザー以外のページだとマイページにリダイレクト
+    end
     @user = current_user
     @user.avatar.attach(params[:avatar_image]) if params[:avatar_image].present?
    # パスワードが入力された場合のみ更新
