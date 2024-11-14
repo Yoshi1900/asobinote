f0f1074f916c7f6c74abd05093b9a0f7d64a4be8
EC2 Default User <ec2-user@ip-172-31-41-40.ap-northeast-1.compute.internal>
2024-11-14 10:38:06 +0000

    デプロイテスト3

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,2 +1,22 @@
 class Admin::SearchesController < ApplicationController
+  before_action :authenticate_admin!
+
+  def search
+    @search_word = params[:word]
+    @range = params[:range]
+    @search_type = params[:search] 
+
+    if @range == "ユーザー"
+      @users = User.looks(params[:search], params[:word])
+    elsif @range == "遊び場"
+      @playgrounds = Playground.looks(params[:search], params[:word])
+    elsif @range == "投稿"
+      @posts = Post.looks(params[:search], params[:word])
+    elsif @range == "タグ"
+      @tags = Tag.looks(params[:search], params[:word])
+    elsif @range == "コメント"
+      @post_comments = PostComment.looks(params[:search], params[:word])
+    else
+    end
+  end
 end
