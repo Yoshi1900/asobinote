d4b69da83086d8166fd2ed69cdbd79162af73bdf
EC2 Default User <ec2-user@ip-172-31-41-40.ap-northeast-1.compute.internal>
2024-11-14 00:20:02 +0000

    taggingsテーブルのpost_idの削除

--- db/migrate/20241110232159_add_post_id_to_taggings.rb
+++
@@ -1,7 +0,0 @@
-class AddPostIdToTaggings < ActiveRecord::Migration[6.0]
-  def change
-    add_column :taggings, :post_id, :integer
-    add_index :taggings, :post_id
-    add_foreign_key :taggings, :posts
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2024_11_10_232160) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2024_11_10_232160) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -101,10 +101,8 @@ ActiveRecord::Schema.define(version: 2024_11_10_232160) do
     t.integer "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "post_id"
     t.index ["playground_id", "tag_id"], name: "index_taggings_on_playground_id_and_tag_id", unique: true
     t.index ["playground_id"], name: "index_taggings_on_playground_id"
-    t.index ["post_id"], name: "index_taggings_on_post_id"
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
@@ -140,6 +138,5 @@ ActiveRecord::Schema.define(version: 2024_11_10_232160) do
   add_foreign_key "posts", "playgrounds"
   add_foreign_key "posts", "users"
   add_foreign_key "taggings", "playgrounds"
-  add_foreign_key "taggings", "posts"
   add_foreign_key "taggings", "tags"
 end
