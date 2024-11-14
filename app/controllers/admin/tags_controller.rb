26f9d09963eebcc8300aec63877670eb9293e4bc
EC2 Default User <ec2-user@ip-172-31-41-40.ap-northeast-1.compute.internal>
2024-11-14 05:28:36 +0000

    テスト3rd

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -4,12 +4,15 @@ class Admin::TagsController < ApplicationController
     @tags = Tag.order(:name).page(params[:page]).per(100)
   end
 
-  def edit
-  end
+   def destroy
+      @tag = Tag.find(params[:id])
+      if @tag.destroy
+        flash[:notice] = 'タグが削除されました。'
+        redirect_to admin_tags_path
+      else
+        flash[:alert] = 'タグの削除に失敗しました。'
+        redirect_to admin_tags_path
+      end
+    end
 
-  def update
-  end
-
-  def destroy
-  end
 end
